0057da6e936b268a4b037c664b3e6ab4fc778b9f
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-03-02 11:34:26 -0500

    Update __init__.py

--- levels/scripts/__init__.py
+++ levels/scripts/__init__.py
@@ -1,4 +1,15 @@
 import json
+
 def on_init(app):
     config = json.load('homeconfig.json')
-    ...
+    for i in config['objects']:
+        obj = app.loader.LoadModel(i['filename'])
+        x = i['x']
+        y = i['y']
+        z = i['z']
+        obj.setPos(x,y,z)
+        obj.reparentTo(app.render)
+        
+    
+    
+        
